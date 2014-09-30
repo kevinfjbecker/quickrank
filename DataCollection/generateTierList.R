5a9ce340b654a9f5cb06c2b310b4b202b30ca12e
Kevin Becker <kevin@Fangorn.local>
2014-10-01 00:41:09 +0200

    Forward to JSON
    Able to output card name and values together with class to JSON

--- DataCollection/generateTierList.R
+++ DataCollection/generateTierList.R
@@ -40,7 +40,7 @@ for(i in 1:ncol(M)){
     
     # Start Collection Group
     if(gActiveRating != -1 & M[j,i] != "") {
-      print(paste("Add ", M[j,i], "to", gRatings[gActiveRating]))
+      # print(paste("Add ", M[j,i], "to", gRatings[gActiveRating])) # DEBUG
       temp <- gRatingsCards[[gActiveRating]]
       gRatingsCards[[gActiveRating]] <- c(temp, M[j,i])
     }
@@ -48,17 +48,37 @@ for(i in 1:ncol(M)){
     # 
     if(!is.na(match(M[j,i], gRatings))) {
       gActiveRating <- match(M[j,i], gRatings)
-      print(paste("Start:", gRatings[gActiveRating]))
+      # print(paste("Start:", gRatings[gActiveRating])) # DEBUG
     }
     
     # End Collection Group
     if(gActiveRating != -1 & M[j,i] == "") {
-      print(paste("End:", gRatings[gActiveRating]))
+      # print(paste("End:", gRatings[gActiveRating])) # DEBUG
       gActiveRating <- -1
     } 
     
   }
 }
 
-#print(paste(length(gTopCards), "'top' cards"))
+# print(paste(length(gTopCards), "'top' cards")) # DEBUG
 
+gCards <- character(0)
+gRatings <- numeric(0)
+
+for(k in 1:length(gRatingsCards)) {
+    for(l in 1:length(gRatingsCards[[k]])) {
+        if(k == 1 | k == 2) {
+            gRatings <- c(gRatings, 9)
+        } else {
+            gRatings <- c(gRatings, 11 - k)
+        }
+        
+        gCards <- c(gCards, gRatingsCards[[k]][[l]])
+    }
+}
+
+gTiers <- data.frame(cbind(gCards, gRatings, "Druid"))
+names(gTiers) <- c("name", "value", "class")
+library(jsonlite)
+
+gJsonOutput <- prettify(toJSON(gTiers))
