e202b73ccc4baf435e5ad4b00cbd566a64ef90ef
Kevin Becker <kevinfjbecker@gmail.com>
2014-12-30 15:47:54 +0100

    Import
    Added Deck Import / Export

--- curves.js
+++ curves.js
@@ -10,6 +10,69 @@ var deck = [],
 
 ///////////////////////////////////////////////////////////////////////////////
 
+function help() {
+  var commands = [
+    'importDeck() // open deck list text input',
+    'loadDeck() // retrieve deck from localStorage',
+    'saveDeck() // put deck in localStorage',
+    'enablePlayTracking() // clicking on a card greys it out',
+    'clearPlayTracking() // un-grey-out all cards'
+  ];
+  commands.forEach(function(c){console.log(c)});
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function importDeck() {
+  $('#decklist-text').attr('rows', 16);
+  $('#decklist-text').val(deckList());
+  $('#myModal').modal();
+}
+
+$('#import-button').click(function(){
+  var cardNames = parseDeckList($('#decklist-text').val());
+  deck = cardNames.map(function(n){return cardByName(n);})
+  updateDeckList(deck);
+  updateCurveCharts(deck);
+  $('#myModal').hide();
+});
+
+function deckList() {
+  var s = '',
+      deckList = d3.map();
+
+  deck.forEach(function(c) {
+    if(deckList.has(c.name)) {
+      deckList.set(c.name, deckList.get(c.name) + 1);
+    } else {
+      deckList.set(c.name, 1);
+    }
+  });
+
+  deckList.forEach(function(card, count){ s += count + ' ' + card + '\n'; });
+
+  return s;
+}
+
+function parseDeckList(s) {
+  var a = s.split('\n').filter(function(s){return s.length > 0;}),
+      d = [],
+      i,
+      n,
+      j;
+
+  for (i = 0; i < a.length; i++) {
+    n = +a[i].substring(0,1);
+    for (j = 0; j < n; j++) {
+      d.push(a[i].substring(2));
+    };
+  };
+
+  return d;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 updateCurveCharts(deck);
 updateDeckList(deck);
 
@@ -47,8 +110,8 @@ function saveDeck() {
 }
 
 function loadDeck() {
-  deckNames = JSON.parse(localStorage.getItem('deck'));
-  deck = deckNames.map(function(n){return cardByName(n);})
+  var cardNames = JSON.parse(localStorage.getItem('deck'));
+  deck = cardNames.map(function(n){return cardByName(n);})
   updateDeckList(deck);
   updateCurveCharts(deck);
 }
